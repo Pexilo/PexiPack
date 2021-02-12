6dfd0a2fe541d240bd88c6609bb65c8c5aceb699
Pexilo <67436391+Pexilo@users.noreply.github.com>
2021-02-12 19:55:46 +0100

    v1.0
    fix DL buton redirect

--- public/javascript/versions.js
+++ public/javascript/versions.js
@@ -23,7 +23,7 @@ function V2_9bis() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/nkpwyygr8ldh2e4/', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000";
+        window.location = "https://pexipack-website.herokuapp.com/accueil";
     })
 };
 
@@ -33,7 +33,7 @@ function V2_9() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/nkpwyygr8ldh2e4/', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -42,7 +42,7 @@ function V2_8() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/z9etibwrrnupetg/', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -51,7 +51,7 @@ function V2_7() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/6egvg7lpt53exn8/%25C2%25A7aPexi%25C2%25A7fPack__%25C2%25A7a%25C2%25A7r%25C2%25A7lv2.7.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -60,7 +60,7 @@ function V2_6() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/jgfjq61hqcisq90/%25C2%25A7aPexi%25C2%25A7fPack__%25C2%25A7a%25C2%25A7r%25C2%25A7lv2.6.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -69,7 +69,7 @@ function V2_5() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/7dmphvy42pg0pgt/%25C2%25A7aPexi%25C2%25A7fPack__%25C2%25A7a%25C2%25A7r%25C2%25A7lv2.5.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -78,7 +78,7 @@ function V2_4() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/rnfltsy0eei2cfu/%25C2%25A7aPexi%25C2%25A7fPack__%25C2%25A7a%25C2%25A7r%25C2%25A7lv2.4.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -87,7 +87,7 @@ function V2_3() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/kwish8b505k5wth/%25C2%25A7aPexi%25C2%25A7fPack__%25C2%25A7a%25C2%25A7r%25C2%25A7lv2.3.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -96,7 +96,7 @@ function V2_2() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/4asm0fdbpmb3ffx/%25C2%25A7aPexi%25C2%25A7fPack__%25C2%25A7a%25C2%25A7r%25C2%25A7lv2.2.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -105,7 +105,7 @@ function V2_1() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/bqeuww62y8r4s81/%25C2%25A7a%25E2%259C%2594Pexi%25C2%25A7fPack__%25C2%25A79%25C2%25A7r%25C2%25A7lv2.1__%25C2%25A7r%25C2%25A76%255B64x%255D.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -114,7 +114,7 @@ function V2_0_3() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/2v3a564hr8n1hhb/%25C2%25A7a%25E2%259C%2594Pexi%25C2%25A7fPack-__%25C2%25A79%25C2%25A7r%25C2%25A7lv2.0.3__%25C2%25A7r%25C2%25A76%255B64x%255D.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -123,7 +123,7 @@ function V2_0_2() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/odph44xd1691nne/%25C2%25A7a%25E2%259C%2594Pexi%25C2%25A7fPack-__%25C2%25A79%25C2%25A7r%25C2%25A7lv2.0.2__%25C2%25A7r%25C2%25A76%255B64x%255D.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -132,7 +132,7 @@ function V2_0_1() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/8bin9uioysd1fec/%25C2%25A7a%25E2%259C%2594Pexi%25C2%25A7fPack-__%25C2%25A79%25C2%25A7r%25C2%25A7lv2.0.1__%25C2%25A7r%25C2%25A76%255B64x%255D.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -141,7 +141,7 @@ function V1_3_7() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/qh21bxoif2o8gt7/%25C2%25A7a%25E2%259C%2594Pexi%25C2%25A7fPack-_%25C2%25A79%25C2%25A7m1.3.7%25C2%25A7r_%25C2%25A7lv2.0%25C2%25A7r_%25C2%25A76%255B64x%255D.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -150,7 +150,7 @@ function V1_3_6() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/c0vvk5skdgwxb66/%25C2%25A7b-Pexi%25C2%25A76Pack-_%25C2%25A79_v1.3.6_%255B32x%255D.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -159,7 +159,7 @@ function V1_3_5() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/ybbc4xanro7kuli/%25C2%25A7b-Pexi%25C2%25A76Pack-_%25C2%25A79_v1.3.5_%25281.10.x%2529.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -168,7 +168,7 @@ function V1_3_4() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/328fpbqw57b2kzc/%25C2%25A7b-Pexi%25C2%25A76Pack-_%25C2%25A79_v1.3.4_%25281.10.x%2529.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -177,7 +177,7 @@ function V1_3_3() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/6fezg7d1k1rkclp/%25C2%25A7b-Pexi%25C2%25A76Pack-_%25C2%25A79_v1.3.3_%25281.10.x%2529.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -186,7 +186,7 @@ function V1_3_2() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/edeah8mb5934dnh/%25C2%25A7b-Pexi%25C2%25A76Pack-_%25C2%25A79_v1.3.2_%25281.10.x%2529.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -195,7 +195,7 @@ function V1_3_1() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/2nf4tggzaot4082/%25C2%25A7b-Pexi%25C2%25A76Pack-_%25C2%25A79_v1.3.1_%25281.10.x%2529.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
 
@@ -204,6 +204,6 @@ function V1_3() {
         // OPEN NEW TAB 
         window.open('https://www.mediafire.com/file/qrotl84xprhi2h6/%25C2%25A7b-Pexi%25C2%25A76Pack-_%25C2%25A79_v1.3_%25281.10.x%2529.zip/file', '_blank');
         // THEN REDIRECT THIS PAGE 
-        window.location = "http://127.0.0.1:3000/Download";
+        window.location = "https://pexipack-website.herokuapp.com/download";
     })
 };
